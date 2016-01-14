1cedd00544d4296128f0068e50875dd6e7011116
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-13 23:23:05 -0800

    edit data and re-add

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function init() {
 	updateContactList();
 	$('form').submit(addContact);
 	$('#contactList').on('click', '.delete', deleteContact);
+	$('#contactList').on('click', '.edit', editContact);
 }
 
 function initializeLocalStorage() {
@@ -20,8 +21,7 @@ function initializeLocalStorage() {
 
 function updateContactList() {
 	var $contactList = $('#contactList');
-	$contactList = $contactList.children().not('#template').empty();
-	console.log($contactList);
+	$contactList = $contactList.children().not('#template').remove();
 	var $contacts = contacts.map(function(contact, index) {
 		var $row = $('#template').clone();
 		$row.removeAttr('id');
@@ -50,14 +50,33 @@ function addData(newContact) {
 	contacts.push(newContact);
 	updateContactList();
 	stringifyContacts();
+	$('#addContact').show(); // Toggle buttons
+	$('#editContact').hide();
 	// $('#contactList').append($tr);
 }
 
 function deleteContact() {
-	var index = $(this).find('tr').index();
-	contacts.splice(index, 1)
+	spliceContact();
 	updateContactList();
 	stringifyContacts();
+}
+
+function editContact() {
+	var $this = $(this);
+	var $editRow = $this.closest('tr').remove();
+	var $rowChildren = $editRow.children();
+	$('#name').val($rowChildren.eq(0).text());
+	$('#tel').val($rowChildren.eq(1).text());
+	$('#email').val($rowChildren.eq(2).text());
+	$('#address').val($rowChildren.eq(3).text());
+	$('#addContact').hide(); // Toggle buttons
+	$('#editContact').show();
+	spliceContact();
+}
+
+function spliceContact(index) {
+	var index = $(this).closest('tr').index();
+	contacts.splice(index - 1, 1);
 }
 
 function stringifyContacts() {
